273021a87ec5609eb7d5a1b29f4b36fed5df7c39
Junior Ruiz <junior31064049@gmail.com>
2022-02-24 21:20:12 -0400

    First commit

---
+++ app.py
@@ -0,0 +1,30 @@
+import pywhatkit
+import datetime
+
+now = datetime.datetime.now()
+hour = now.hour
+minute = int(now.minute)
+
+enviar_otro = 'si'
+
+def datos():
+	number = input('Ingrese número de Teléfono destino: ')
+	cero = number[0]
+	msg = input('Escriba su mensaje: ')
+	if cero == '0':
+		number = number.replace(cero, '+58', 1)
+	return number, msg
+
+def send_message(number, msg):
+	try:
+		pywhatkit.sendwhatmsg_instantly(number, msg, 5)
+		print(f'Mensaje Enviado -> {number} Hora -> {hour}:{minute}')
+	except Exception as e:
+		print('No se pudo Enviar el Mensaje')
+		print(e)		
+
+while enviar_otro == 'si':
+	number, msg = datos()
+	send_message(number, msg)
+	enviar_otro = input('¿Desea enviar otro mensaje?: ')
+	enviar_otro = enviar_otro.lower()
